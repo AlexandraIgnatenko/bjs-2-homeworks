b600c49164915604daa87ea9cfc253a716e8899f
Alexandra_Ignatenko <9val_2004@mail.ru>
2023-01-06 16:21:06 +1000

    решено задание 4

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,36 @@
 function Student(name, gender, age) {
-  
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
 }
 
 Student.prototype.setSubject = function (subjectName) {
-  
+    this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+    if (Student.hasOwnProperty('excluded') === true) {
+        return;
+    }
+    if (this.marks !== undefined) {
+        this.marks.push(...marks);
+    } 
 }
 
 Student.prototype.getAverage = function () {
-  
+    if (this.marks.length === 0) {
+        return 0;
+    }
+    let sum = 0;
+    for (let index = 0; index < this.marks.length; index++) {
+        sum += this.marks[index];
+    }
+    return sum / this.marks.length;
 }
 
 Student.prototype.exclude = function (reason) {
-  
+    this.excluded = reason;
+    delete this.subject;
+    delete this.marks;
 }
