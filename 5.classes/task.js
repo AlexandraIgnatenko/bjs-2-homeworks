d58014eed8053d9d8ea485674df06a1e2e9527a5
Alexandra_Ignatenko <9val_2004@mail.ru>
2023-01-08 03:23:13 +1000

    Выполнено задание 5

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -80,7 +80,7 @@ class Library {
 
     findBookBy(type, value) {
         for(let i = 0; i < this.books.lenght; i++) {
-            if(this.books[i].type === value) {
+            if(this.books[i][type] === value) {
                 return this.books[i];
             } else {
                 return null;
