ace01ad33d60f9e6c505a8db0870214085535815
Alexandra_Ignatenko <9val_2004@mail.ru>
2023-01-20 20:17:58 +1000

    Выполнено задание 8 задача 1

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,32 @@
-//Задача № 1
+"use strict"
+
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    return function(...args) {
+        const hash = md5(args);
+        let objectInCache = cache.find((objectInCache) => objectInCache.hash === hash);
+
+        if(objectInCache) {
+            console.log("Из кэша: " + objectInCache.value);
+            return "Из кэша: " + objectInCache.value;
+        }
+
+        let value = func(...args);
+        cache.push({hash, value});
+
+        if(cache.length > 5) {
+            cache.shift();
+        }
+
+        console.log("Вычисляем: " + value);
+        return "Вычисляем: " + value;
+    }
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    const sendSignal = (signalOrder, delay) => console.log("Сигнал отправлен", signalOrder, delay);
+    const upgradedSendSignal = debounceDecoratorNew(sendSignal, 2000);
+    
 }
